309c56c7195f8540780afd52e3a74a150580c0f3
ashwinib23 <ashwinibasavaraju005@gmail.com>
2023-11-20 11:03:26 +0530

    2ndcommit

--- practiseforgovt/src/browser/Launchbrowser.java
+++ practiseforgovt/src/browser/Launchbrowser.java
@@ -8,22 +8,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Launchbrowser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		//System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
-
-		//---------------------------------------------------------------------
-//		ChromeOptions options= new ChromeOptions();
-//		options.addArguments("--remote-allow-origins=*");
-//		WebDriver driver =new ChromeDriver(options);
-		WebDriver driver =new ChromeDriver();
 		
-		//---------------------------------------------------------------------
-		//WebDriver driver =new FirefoxDriver();
-
-		//--------------------------------------------------------------------------
-		driver.get("https://www.amazon.com/"); 
-		//--------------------------------------------------------------------------------
+		
 		//1st commit
 
 	}
